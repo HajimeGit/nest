dd371ea12b9e0ea417571761ccd0ca6b9c80de10
hajime <t.korpach@internetdevels.com>
2024-05-27 14:28:02 +0300

    Initial setup

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+  logging: process.env.DATABASE_LOGGING === 'true',
+}));
---
+++ src/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from '../config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+  ],
+})
+export class DatabaseModule {}
